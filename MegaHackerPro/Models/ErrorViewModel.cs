13bb47963a4b883f9622f272da1483e85b66d8df
Hesu <davidxxdavid7@gmail.com>
2021-03-14 01:54:19 -0800

    Add project files.

---
+++ MegaHackerPro/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MegaHackerPro.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
